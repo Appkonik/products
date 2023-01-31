7452737db2715a427bd4e98f84553be1c3d6037a
SherifSheremetajRitech <shsheremetaj@ritech.co>
2023-01-31 13:12:52 +0100

    Added crud methods

--- src/Products.php
+++ src/Products.php
@@ -1,12 +1,50 @@
 <?php namespace Appkonik\Products;
 
-use Illuminate\Support\Facades\Http;
+use Appkonik\Products\Models\Product;
+use Exception;
 
 class Products
 {
-    public function justDoIt() {
-        $response = Http::get('https://inspiration.goprogram.ai/');
+    public function get($id): string|Product
+    {
+        try {
+            $product = Product::findOrFail($id);
+            return $product;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
-        return $response['quote'] . ' -' . $response['author'];
+    public function add($data): string|Product
+    {
+        try {
+            $product = new Product();
+            $product->fill($data)->save();
+            return $product;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function update($id, $data): string|Product
+    {
+        try {
+            $product = Product::findOrFail($id);
+            $product->fill($data)->save();
+            return $product;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function remove($id): bool|string
+    {
+        try {
+            $product = Product::findOrFail($id);
+            $product->delete();
+            return true;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
