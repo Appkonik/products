39ef232a26e9547d3231dfb5b000e36f906504dd
SherifSheremetajRitech <shsheremetaj@ritech.co>
2023-01-31 14:49:06 +0100

    Added getAll and get with pagination methods

--- src/Products.php
+++ src/Products.php
@@ -2,10 +2,11 @@
 
 use Appkonik\Products\Models\Product;
 use Exception;
+use Illuminate\Database\Eloquent\Collection;
 
 class Products
 {
-    public function get(int $id): string|Product
+    public function getById(int $id): string|Product
     {
         try {
             $product = Product::findOrFail($id);
@@ -14,6 +15,17 @@ class Products
             return $e->getMessage();
         }
     }
+
+    public function getAll(): Collection
+    {
+        return Product::all();
+    }
+
+    public function get(int $page_size = 20)
+    {
+        return Product::paginate($page_size);
+    }
+
 
     public function add(array $data): string|Product
     {
